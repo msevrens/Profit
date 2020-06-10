61e2b74cf60f2c8b21b2b12f45cd53d14ec20869
Matt Sevrens <msevrens@gmail.com>
2020-06-09 17:18:23 -0700

    keeping old code in tandem with new for training for a bit

--- profit/train_model.py
+++ profit/train_model.py
@@ -12,7 +12,8 @@ Created on May 25, 2020
 
 #################### USAGE ##########################
 
-# python3 -m profit.train_model
+# python3 -m profit.train_model [mode] [model]
+# python3 -m profit.train_model test models/model_13K-V_20K-T.zip
 
 #####################################################
 
@@ -31,61 +32,98 @@ from stable_baselines import PPO2
 from profit.multi_stock_env import MultiStockEnv
 from profit.get_market_data import load_dow_data, get_historical_prices
 
+def test_model(model_file):
+    """Test model"""
 
-# Load Data
-# train_data = load_dow_data(True)
-# test_data = load_dow_data(False)
+    # Load Data and Environments
+    validation_data = get_historical_prices(time_frame=("2016-01-01", "2018-09-30"))
+    test_data = get_historical_prices(time_frame=("2018-09-30", "2020-04-02"))
+    validationEnv = DummyVecEnv([lambda: MultiStockEnv(validation_data, train=False)])
+    testEnv = DummyVecEnv([lambda: MultiStockEnv(test_data, train=False)])
 
-train_data = get_historical_prices(time_frame=("2000-01-01", "2009-12-31"))
-validation_data = get_historical_prices(time_frame=("2010-01-01", "2014-12-31"))
-test_data = get_historical_prices(time_frame=("2015-01-01", "2020-04-02"))
+    print("Number of days in validation data: " + str(len(validation_data)))
+    print("Number of days in test data: " + str(len(test_data)) + "\n")
 
-print("Number of days in train data: " + str(len(train_data)))
-print("Number of days in test data: " + str(len(validation_data)))
+    # Load Model if Necessary
+    if type(model_file) == str:
+        model = PPO2(MlpPolicy, validationEnv, verbose=1)
+        model.load(model_file, env=validationEnv)
+    else:
+        model = model_file
 
-# Vectorize Environment
-trainEnv = DummyVecEnv([lambda: MultiStockEnv(train_data)])
-model = PPO2(MlpPolicy, trainEnv, verbose=1)
-model.learn(total_timesteps=25000)
-obs = trainEnv.reset()
-
-# Validate
-validationEnv = DummyVecEnv([lambda: MultiStockEnv(validation_data, train=False)])
-
-results = []
-
-for ii in range(10):
-    for i in range(len(validation_data) - 1):
-        # print("Step: " + str(i))
-        action, _states = model.predict(obs)
-        obs, rewards, done, info = validationEnv.step(action)
-        # testEnv.render(mode="test")
-    print("Validation run #" + str(ii))
-    total_reward = validationEnv.render(mode="test", suffix="_validation_" + str(ii))
-    results.append(total_reward)
+    results = []
+    model.set_env(validationEnv)
     obs = validationEnv.reset()
 
-print(results)
-print("\nAverage Reward: " + str(sum(results) / len(results)))
+    # Validate
+    for i in range(10):
+        for j in range(len(validation_data) - 1):
+            # print("Step: " + str(j))
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = validationEnv.step(action)
+            # validationEnv.render(mode="test")
+        print("Validation run #" + str(i))
+        total_reward = validationEnv.render(mode="test", suffix="_validation_" + str(i))
+        results.append(total_reward)
+        obs = validationEnv.reset()
 
-# Test
-testEnv = DummyVecEnv([lambda: MultiStockEnv(test_data, train=False)])
-
-results = []
-
-for ii in range(10):
-    for i in range(len(test_data) - 1):
-        # print("Step: " + str(i))
-        action, _states = model.predict(obs)
-        obs, rewards, done, info = testEnv.step(action)
-        # testEnv.render(mode="test")
-    print("Test run #" + str(ii))
-    total_reward = testEnv.render(mode="test", suffix="_test_" + str(ii))
-    results.append(total_reward)
+    print(results)
+    print("\nAverage Reward: " + str(sum(results) / len(results)))
+    results = []
+    model.set_env(testEnv)
     obs = testEnv.reset()
 
-print(results)
-print("\nAverage Reward: " + str(sum(results) / len(results)))
+    # Test
+    for i in range(10):
+        for j in range(len(test_data) - 1):
+            # print("Step: " + str(j))
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = testEnv.step(action)
+            # testEnv.render(mode="test")
+        print("Test run #" + str(i))
+        total_reward = testEnv.render(mode="test", suffix="_test_" + str(i))
+        results.append(total_reward)
+        obs = testEnv.reset()
 
-# Save Final Model
-model.save("models/model")
+    print(results)
+    print("\nAverage Reward: " + str(sum(results) / len(results)))
+
+    return None
+
+def train_model():
+    """Train model"""
+
+    # Load Data and Environment
+    train_data = get_historical_prices(time_frame=("2009-01-01", "2015-12-31"))
+    trainEnv = DummyVecEnv([lambda: MultiStockEnv(train_data)])
+
+    print("Number of days in train data: " + str(len(train_data)) + "\n")
+
+    # Init Model
+    model = PPO2(MlpPolicy, trainEnv, verbose=1)
+
+    # Train Model
+    model.learn(total_timesteps=25000)
+    obs = trainEnv.reset()
+
+    # Test Model
+    test_model(model)
+
+    # Save Final Model
+    model.save("models/model")
+
+    return None
+
+def main():
+    """Run module from command line"""
+
+    mode = sys.argv[1] if len(sys.argv) > 1 else None
+    model_file = sys.argv[2] if len(sys.argv) > 2 else None
+
+    if mode == "test":
+        test_model(model_file)
+    else:
+        train_model()
+
+if __name__ == "__main__":
+    main()
---
+++ profit/train_model_legacy.py
@@ -0,0 +1,92 @@
+#!/usr/local/bin/python3
+
+"""This module trains a profit agent in an OpenAI Gym environment
+
+Created on May 25, 2020
+@author: Matthew Sevrens
+"""
+
+# https://towardsdatascience.com/creating-a-custom-openai-gym-environment-for-stock-trading-be532be3910e
+# https://github.com/bioothod/gym-stocks
+# https://github.com/notadamking/Stock-Trading-Environment
+
+#################### USAGE ##########################
+
+# python3 -m profit.train_model
+
+#####################################################
+
+import sys
+import json
+import datetime as dt
+
+import pandas as pd
+import numpy as np
+import gym
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+
+from profit.multi_stock_env import MultiStockEnv
+from profit.get_market_data import load_dow_data, get_historical_prices
+
+
+# Load Data
+train_data = load_dow_data(True)
+test_data = load_dow_data(False)
+
+# train_data = get_historical_prices(time_frame=("2009-01-01", "2015-12-31"))
+validation_data = get_historical_prices(time_frame=("2018-09-30", "2020-04-01"))
+# test_data = get_historical_prices(time_frame=("2018-09-30", "2020-04-02"))
+
+print("Number of days in train data: " + str(len(train_data)))
+print("Number of days in test data: " + str(len(test_data)))
+print("Number of days in validation data: " + str(len(validation_data)))
+
+# Vectorize Environment
+trainEnv = DummyVecEnv([lambda: MultiStockEnv(train_data)])
+model = PPO2(MlpPolicy, trainEnv, verbose=1)
+model.learn(total_timesteps=25000)
+obs = trainEnv.reset()
+
+# Validate
+validationEnv = DummyVecEnv([lambda: MultiStockEnv(validation_data, train=False)])
+
+results = []
+
+for ii in range(10):
+    for i in range(len(validation_data) - 1):
+        # print("Step: " + str(i))
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = validationEnv.step(action)
+        # testEnv.render(mode="test")
+    print("Validation run #" + str(ii))
+    total_reward = validationEnv.render(mode="test", suffix="_validation_" + str(ii))
+    results.append(total_reward)
+    obs = validationEnv.reset()
+
+print(results)
+print("\nAverage Reward: " + str(sum(results) / len(results)))
+
+# Test
+testEnv = DummyVecEnv([lambda: MultiStockEnv(test_data, train=False)])
+
+results = []
+
+for ii in range(10):
+    for i in range(len(test_data) - 1):
+        # print("Step: " + str(i))
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = testEnv.step(action)
+        # testEnv.render(mode="test")
+    print("Test run #" + str(ii))
+    total_reward = testEnv.render(mode="test", suffix="_test_" + str(ii))
+    results.append(total_reward)
+    obs = testEnv.reset()
+
+print(results)
+print("\nAverage Reward: " + str(sum(results) / len(results)))
+
+# Save Final Model
+model.save("models/model")
